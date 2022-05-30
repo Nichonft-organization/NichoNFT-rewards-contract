70877c7d5c497a207759ce6a3132535766d7daf0
looks <looks@gmail.com>
2022-05-30 20:19:40 +0800

    add REWARDS_ROLE variable

--- contracts/NichoNFTRewards.sol
+++ contracts/NichoNFTRewards.sol
@@ -6,14 +6,18 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NichoNFTRewards is Ownable {
+contract NichoNFTRewards is Ownable, AccessControl {
     using SafeMath for uint256;
+    
+    bytes32 public constant REWARDS_ROLE = keccak256("REWARDS_ROLE");
 
     bool public claimEnabled = false;
     address public rewardsToken;
     uint256 public lastUpdatedNo;
-    uint256 public totalRewardsCount;
+    uint256 public totalUserRewardsCount;
+    uint256 public totalUserRewardsAmount;
     mapping (address => RewardItems) private userRewards;
 
     struct RewardItems {
@@ -23,11 +27,13 @@ contract NichoNFTRewards is Ownable {
         uint256 time;
     }
 
-    event rewardsUpdated(uint256 no, uint256 amountSum, uint256 userCount, uint256 time);
-    event claim(uint256 no, address user, uint256 amount, uint256 time);
+    event RewardsUpdated(uint256 no, uint256 amountSum, uint256 userCount, uint256 time);
+    event Claim(uint256 no, address user, uint256 amount, uint256 time);
+    event Transfer(address erc20Address, address to, uint256 amount, uint256 time);
 
     constructor (address _rewardsToken) {
         rewardsToken = _rewardsToken;
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
     function getUserRewardsAmount(address user) public view returns (uint256) {
@@ -53,10 +59,10 @@ contract NichoNFTRewards is Ownable {
         items.amount = 0;
         token.transfer(_msgSender(), userAmount);
 
-        emit claim(items.no, _msgSender(), userAmount, block.timestamp);
+        emit Claim(items.no, _msgSender(), userAmount, block.timestamp);
     }
 
-    function updateRewards(RewardItems[] calldata  _rewardsItmes) public onlyOwner {
+    function updateRewards(RewardItems[] calldata  _rewardsItmes) public onlyRole(REWARDS_ROLE) {
         require(_rewardsItmes.length > 0, "NichoNFTRewards: no rewards data");
         uint256 amountSum = 0;
         uint256 userCount = 0;
@@ -69,8 +75,17 @@ contract NichoNFTRewards is Ownable {
             // set user mapping
             userRewards[itmes.user] = RewardItems({no: itmes.no, user: itmes.user, amount: itmes.amount, time: itmes.time});
         }
-        totalRewardsCount = userCount;
-        emit rewardsUpdated(lastUpdatedNo, amountSum, userCount, block.timestamp);
+        totalUserRewardsCount = userCount;
+        totalUserRewardsAmount = amountSum;
+        emit RewardsUpdated(lastUpdatedNo, amountSum, userCount, block.timestamp);
+    }
+
+    function transferToken(address _erc20Address, address _to, uint256 _amount) public onlyOwner {
+        IERC20 erc20 = IERC20(_erc20Address);
+        uint balance = erc20.balanceOf(address(this));
+        require(balance >= _amount, "NichoNFTRewards: invalid amount");
+        erc20.transfer(_to, _amount);
+        emit Transfer(_erc20Address, _to, _amount, block.timestamp);
     }
 
     function setRewardsToken(address _rewardsToken) public onlyOwner {
